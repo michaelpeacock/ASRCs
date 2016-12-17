f201e9b1ffaa5d48fff7bd948ec2741c2cb5a5ab
justinrlok <justinrlok@gmail.com>
2016-12-16 21:43:45 -0500

    List Data Struct Added

--- ASRCsCodeathonProject/ASRCsCodeathonProject/ListSerializableDataStruct.cs
+++ ASRCsCodeathonProject/ASRCsCodeathonProject/ListSerializableDataStruct.cs
@@ -9,7 +9,7 @@ using System.Globalization;
 using System.Collections;
 
 [Serializable()]
-class SerializableDataStruct : ISerializable
+class ListSerializableDataStruct : ISerializable
 {
     string date;
     string time;
@@ -31,55 +31,59 @@ class SerializableDataStruct : ISerializable
     DateTime parsedDate;
     DateTime parsedTime;
 
-    public SerializableDataStruct(string in_date, string in_time, string in_mag_dir, string in_true_dir, double in_wind_speed, 
-                            double in_cross_wind, double in_head_wind, double in_temp, double in_wind_chill, double in_rel_hum, 
+    public ListSerializableDataStruct(string in_date, string in_time, string in_mag_dir, string in_true_dir, double in_wind_speed,
+                            double in_cross_wind, double in_head_wind, double in_temp, double in_wind_chill, double in_rel_hum,
                             double in_heat_index, double in_dew_point, double in_wet_bulb, double in_bar, double in_alt, double in_den_alt)
     {
         if (DateTime.TryParseExact(in_date, "MM/dd/yyyy", null, DateTimeStyles.None, out parsedDate))
-            date = in_date.Substring(0, 9);      
-        else      
+        {
+            date = in_date.Substring(0, 9);
+        }
+        else
+        {
             date = "ERROR";
-      
+        }
 
-        if (DateTime.TryParseExact(in_time, "tt:h:mm:ss", null, DateTimeStyles.None, out parsedTime))      
-            time = in_time.Substring(11, 21);   
-        else      
+
+        if (DateTime.TryParseExact(in_time, "tt:h:mm:ss", null, DateTimeStyles.None, out parsedTime))
+        {
+            time = in_time.Substring(11, 21);
+        }
+        else
+        {
             time = "ERROR";
-       
+        }
 
-        if (in_mag_dir != null)      
-            mag_dir = in_mag_dir;      
-        else      
+        if (in_mag_dir != null)
+        {
+            mag_dir = in_mag_dir;
+        }
+        else 
+        {
             mag_dir = "NULL";
+        }
 
 
         if (in_true_dir != null)
+        {
             true_dir = in_true_dir;
+        }
         else
+        {
             true_dir = "NULL";
+        }
 
         wind_speed = in_wind_speed;
-
         cross_wind = in_cross_wind;
-
         head_wind = in_head_wind;
-
         temp = in_temp;
-
         wind_chill = in_wind_chill;
-
         rel_hum = in_rel_hum;
-
         heat_index = in_heat_index;
-
         dew_point = in_dew_point;
-
         wet_bulb = in_wet_bulb;
-
         bar = in_bar;
-
         alt = in_alt;
-
         den_alt = in_den_alt;
     }
 
@@ -94,9 +98,9 @@ class SerializableDataStruct : ISerializable
 
     // Implement this method to serialize data. The method is called 
     // on serialization.
+    // Use the AddValue method to specify serialized values.
     public void GetObjectData(SerializationInfo info, StreamingContext context)
     {
-        // Use the AddValue method to specify serialized values.
         info.AddValue("date", date);
         info.AddValue("time", time);
         info.AddValue("mag_dir", mag_dir);
@@ -116,7 +120,7 @@ class SerializableDataStruct : ISerializable
     }
 
     // The special constructor is used to deserialize values.
-    public SerializableDataStruct(SerializationInfo info, StreamingContext context)
+    public ListSerializableDataStruct(SerializationInfo info, StreamingContext context)
     {
         // Reset the property value using the GetValue method.
         myProperty_value = (string)info.GetValue("props", typeof(string));
@@ -126,19 +130,9 @@ class SerializableDataStruct : ISerializable
     {
         //string value = date + " " + time;
 
-        return "Date: " + this.date + ", Time: " + this.time + ", Magnetic Direction: " + this.mag_dir + ", True Direction: " + this.true_dir 
+        return "Date: " + this.date + ", Time: " + this.time + ", Magnetic Direction: " + this.mag_dir + ", True Direction: " + this.true_dir
                 + ", Wind Speed: " + this.wind_speed + ", Cross Wind: " + this.cross_wind + ", Head Wind: " + this.head_wind + ", Temperature: "
                 + ", Wind Chill: " + this.wind_chill + ", Relative Humidity: " + this.rel_hum + ", Heat Index: " + this.heat_index + ", Dew Point: "
                 + ", Wet Bulb: " + this.wet_bulb + ", Barometric Pressure: " + this.bar + ", Altitude: " + this.alt + ", Density Altitude: " + this.den_alt;
-    }
-
-    public override bool Equals(object obj)
-    {
-        return base.Equals(obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
     }
 }
